a6f660247e8a8c4397e2c3234509a91bc5bef50d
Jason Vennin <jason.vennin@gmail.com>
2021-04-30 15:17:05 +0200

    ajout du js sur le carousel

---
+++ scripts/script.inspiration.js
@@ -0,0 +1,9 @@
+var moveScrollingLeft = () => {
+    let maDiv = document.getElementsByClassName("carousel_container")[0];
+    maDiv.scrollLeft = 2000;
+}
+
+var moveScrollingRight = () => {
+    let maDiv = document.getElementsByClassName("carousel_container")[0];
+    maDiv.scrollLeft= 0;
+}
